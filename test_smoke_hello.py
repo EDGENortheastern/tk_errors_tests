3a20e628353448c5b1d347a90be92d6c106797cf
Katia <katia.punter@storyshareplatform.com>
2023-10-10 14:14:48 +0100

    Adds smoke

--- test_smoke_hello.py
+++ test_smoke_hello.py
@@ -1,22 +1,25 @@
 import unittest # 🔴 importing unittest
-from test_start import HelloName
 
 class SmokeTest(unittest.TestCase):
     def test_ut_works(self):
+        # Assert that 2 + 2 is equal to 4
         self.assertEqual(2+2,4)
-
-class TestHelloName(unittest.TestCase):
-
-    def setUp(self):
-        self.app = HelloName(init_gui=False)
-
-    # Tests for decorate_name function
-    def test_decorate_name(self):
-        app = HelloName()
-        decorated = app.decorate_name("John")
-        self.assertEqual(decorated, "Hello, 🌿🌼 John 🌼🌿!")
-        self.assertTrue(decorated.startswith("Hello,"))
-        self.assertIsInstance(app.decorate_name("alice"), str)
+        
+        # Assert that the value is True (truthy)
+        self.assertTrue(True)
+        
+        # Assert that the value is False (falsy)
+        self.assertFalse(0)
+    
+    def test_ut_works_with_more(self):
+        # Assert that 5 is greater than 4
+        self.assertGreater(5,4)
+        
+        # Assert that "Me" is an instance of the str (string) class
+        self.assertIsInstance("Me", str)
+        
+        # Assert that the letter "c" is not present in the word "lion"
+        self.assertNotIn("c", "lion")
 
 if __name__ == "__main__":
     unittest.main(verbosity=3)
